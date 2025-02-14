5ebb84b74d8c5a2b6259e793f33683a83a4b5b2b
luuna <luna@hackclub.app>
2025-02-14 09:04:37 +0800

    super basic screen

--- editor.py
+++ editor.py
@@ -1,5 +1,59 @@
+import re
+
 class Editor:
-    def __init__(self, filename, buffer = [], line=0):
+    def __init__(self, filepath, buffer = [], line=0):
         self.buffer = buffer
-        self.filename = filename
+        self.filepath = filepath
         self.currentline = line
+
+        try:
+            with open(filepath, 'r') as file:
+                self.buffer = file.read().splitlines()
+        except FileNotFoundError:
+            self.buffer = []
+
+
+    def printbuffer(self):
+        print("\n--- File Buffer ---")
+        for i, line in enumerate(self.buffer):
+            prefix = "-> " if i == self.currentline else "   "
+            print(f"{prefix}{i+1}: {line}")
+        print("-------------------\n")
+
+    def run(self):
+        print("Welcome to VimLikeEditor (a minimal vim-like editor).")
+        while True:
+            self.printbuffer()
+            cmd = input(":")
+            if cmd.startswith(":"):
+                self.handlecolon(cmd)
+            elif cmd.startswith("/"):
+                self.handlesearch(cmd)
+            elif cmd == "i":
+                self.handleinsert()
+            elif re.match(r'^d(\d*)d$', cmd):
+                self.handledelete(cmd)
+            elif cmd == "j":
+                self.movedown()
+            elif cmd == "k":
+                self.moveup()
+            else:
+                print("unknown command. supported commands: :w, :q, :wq, /pattern, i, dd, dNd, j, k.")
+
+    def handlecolon(self, cmd):
+        pass
+
+    def handlesearch(self, cmd):
+        pass
+
+    def handleinsert(self):
+        pass
+
+    def handledelete(self, cmd):
+        pass
+
+    def movedown(self):
+        pass
+
+    def moveup(self):
+        pass
--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import sys
+from editor import Editor
 
 def main():
     args = sys.argv[1:]
-    match len(args):
-        case 0:
-            # TODO: handle default config
-            pass
-        case 1:
-            if args[0] == "config":
-                # handle config
-                pass
-            else:
-                pass
-                # TODO
-        case _:
-            # TODO: handle others
-            pass
+    editor = Editor(filepath=args[0])
+
+    editor.run()
+    # match len(args):
+    #     case 0:
+    #         # TODO: handle default config
+    #         pass
+    #     case 1:
+    #         if args[0] == "config":
+    #             # handle config
+    #             pass
+    #         else:
+    #             pass
+    #             # TODO
+    #     case _:
+    #         # TODO: handle others
+    #         pass
 
 if __name__ == "__main__":
     main()
