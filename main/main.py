883c994eed0d393366a8f35a15be4753edd553db
luuna <luna@hackclub.app>
2025-02-21 18:27:00 +0800

    added install script

--- main/main.py
+++ main/main.py
@@ -1,18 +1,18 @@
 from sys import argv
+from typing import Any, List
 from os import makedirs
 from os.path import expanduser, isdir
 
 from editor import Editor
 
-def main():
-    usrhome = expanduser("~")
-    args = argv[1:]
+def main() -> None:
+    usrhome: str = expanduser("~")
+    args: List[Any] = argv[1:]
     if not isdir(f"{usrhome}/.config/scrappad/"):
         makedirs(f"{usrhome}/.config/scrappad/")
+    editor: Editor = Editor(f"{usrhome}/.config/scrappad/notes.txt")
     if args:
         editor = Editor(filepath=args[0])
-    else:
-        editor = Editor(f"{usrhome}/.config/scrappad/notes.txt")
 
     editor.run()
 
